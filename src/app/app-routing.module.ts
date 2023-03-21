7c27122647c6dda7fafd0902658950f0e0ee9896
rri <raul.rincones@adorsys.com>
2023-03-21 13:34:30 +0100

    Side bar and navigation Fertig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EquipmentComponent } from './pages/equipment/equipment.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'profile',
+    component: AppComponent,
+  },
+  {
+    path: 'equipment',
+    component: AppComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
